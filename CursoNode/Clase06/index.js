f6de13a74e0826f58ec85e6a54ec92593f804488
efileonardo-creator <efi.leonardo@gmail.com>
2026-09-12 08:46:57 -0300

    COntenidos teoricos OK

--- CursoNode/Clase06/index.js
+++ CursoNode/Clase06/index.js
@@ -43,3 +43,44 @@ taskAsync2()
 .finally(() => console.log('Fin de la tarea.')); 
 
 
+/** Metoro async/await */
+async function executeAsyncTask () { 
+    console.log('Inicio de la tarea.'); 
+    try { 
+        const result = await taskAsync(); 
+        console.log(result); 
+    } catch (error) { 
+      console.log(error); 
+    } finally { 
+        console.log('Fin de la tarea.') 
+} 
+} 
+executeAsyncTask(); 
+
+
+/** Fetch */ 
+fetch ('./datos.json') 
+.then((response) => response.json()) 
+.then((data) => console.log(data))
+
+/** Otras funcionalidades importantes de Fetch: 
+1. Configuración avanzada: Puedes personalizar las solicitudes agregando un objeto 
+de configuración. Por ejemplo, configurar los encabezados HTTP, enviar datos en el 
+cuerpo de una solicitud POST o definir el método HTTP: 
+2. Soporte para cookies y autenticación: Usando la opción credentials, Fetch 
+puede manejar cookies o enviar credenciales entre dominios. */
+
+const config = { 
+    method: 'POST', 
+    headers: { 
+        'Content-Type': 'application/json', 
+        'Authorization': 'Bearer token', 
+    }, 
+    body: JSON.stringify({ key: 'value' }), 
+}; 
+fetch('https://api.example.com/data', config) 
+.then(response => response.json()) 
+.then(data => console.log(data)) 
+//  El error debe manejarse verificando manualmente response.ok. 
+.catch(error => console.error('Error:', error)); 
+
